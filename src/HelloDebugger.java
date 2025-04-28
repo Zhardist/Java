fffbbe4e36e8c24014c06e81d6dfaaa7457fecd5
Jan Grützke <jan.gruetzke@gmail.com>
2025-04-28 12:00:41 +0200

    HelloDebugger

---
+++ src/HelloDebugger.java
@@ -0,0 +1,12 @@
+public class HelloDebugger {
+        public static void main(String[] args){
+            int a = 6;
+            int b = 7;
+
+            //calculate the product
+        int product = 6 * 7;
+
+        //print the result
+        System.out.println(a+"*"+b+"="+product);
+    }
+}
